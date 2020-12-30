33b62a93a542494239ca6fe4611f1d6bafe654bb
Jorge Olarte <ingeniero@jorgeolarte.com>
2020-12-30 15:08:51 -0500

    Delete redux-persist

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { ReduxNetworkProvider } from "react-native-offline";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./src/store";
+import { store } from "./src/store";
 import MyApp from "./src/index";
 
 export default function () {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ReduxNetworkProvider
-          pingTimeout={1000}
-          pingServerUrl='https://google.com'
-          pingInterval={30000}
-          httpMethod='HEAD'
-          pingInBackground={true}
-          pingOnlyIfOffline={true}
-        >
-          <MyApp />
-        </ReduxNetworkProvider>
-      </PersistGate>
+      <ReduxNetworkProvider
+        pingTimeout={1000}
+        pingServerUrl='https://google.com'
+        pingInterval={30000}
+        httpMethod='HEAD'
+        pingInBackground={true}
+        pingOnlyIfOffline={true}
+      >
+        <MyApp />
+      </ReduxNetworkProvider>
     </Provider>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,7 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import { persistStore, persistReducer } from "redux-persist";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { reducer as network } from "react-native-offline";
 import { createNetworkMiddleware } from "react-native-offline";
 import * as reducers from "./reducers";
@@ -11,20 +9,9 @@ const rootReducer = combineReducers({
   network,
 });
 
-const persistConfig = {
-  key: "root",
-  storage: AsyncStorage,
-  timeout: 0,
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 const networkMiddleware = createNetworkMiddleware();
 
 export const store = createStore(
-  persistedReducer,
+  rootReducer,
   compose(applyMiddleware(thunk, networkMiddleware))
-  // applyMiddleware(thunk, networkMiddleware)
 );
-
-export const persistor = persistStore(store);
