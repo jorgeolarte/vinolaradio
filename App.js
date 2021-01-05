c7adabdec6c24b7cc14ac0564d29859f351f37ec
Jorge Olarte <ingeniero@jorgeolarte.com>
2021-01-05 09:08:54 -0500

    Adding Sentry

--- App.js
+++ App.js
@@ -3,6 +3,15 @@ import { Provider } from "react-redux";
 import { ReduxNetworkProvider } from "react-native-offline";
 import { store } from "./src/store";
 import MyApp from "./src/index";
+import * as Sentry from "sentry-expo";
+import { SENTRY_DNS } from "@env";
+
+Sentry.init({
+  dsn: SENTRY_DNS,
+  enableInExpoDevelopment: true,
+  debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
+  tracesSampleRate: 0.5,
+});
 
 export default function () {
   return (
